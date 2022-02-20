79b0fa5428dff053519ae013b53cf7f25fe2cc22
huoyang11 <2825710199@qq.com>
2022-02-20 21:06:44 +0800

    添加遍历list、dict、list的案例

--- pytest.py
+++ pytest.py
@@ -1,6 +1,24 @@
+width = 20
+teststr = "asdasd"
+testfloat = 2.2
+testlist = [2,3,4,5,6]
+testtuple = ("aaa","bbb","ccc")
+testdict = {"asd":2,"lll":555,"bsd":444}
+
 def testc(a,b):
     print("a + b =",a + b)
+    print(height)
+    print(width)
     return a + b
+
+def mk(a,b):
+    print("a =",a)
+    print("b =",b)
+    print(mki)
+    print(mkd)
+    print(mks)
+    print(mktuple)
+    print(mklist)
 
 def main():
     testc(2,6)
--- python3call.c
+++ python3call.c
@@ -1,14 +1,26 @@
 #include <Python.h>
 
-int main(int argc, char *argv[]) 
+int test(int argc, char *argv[]) 
 {
 	Py_Initialize();
 	//PyRun_SimpleString("print('hello world')\n");
 	PyRun_SimpleString("import os,sys");
-    PyRun_SimpleString("sys.path.append('./')");
+	PyRun_SimpleString("sys.path.append('./')");
 
 	PyObject *module = PyImport_ImportModule("pytest");
 	PyObject *function = PyObject_GetAttrString(module,"testc");
+
+	PyObject *width = PyObject_GetAttrString(module,"width");
+
+	if (PyLong_Check(width))
+	{
+		long val = PyLong_AsLong(width);
+		printf("width = %ld\n",val);
+	}
+
+	int height = 77;
+	PyObject_SetAttrString(module,"height",Py_BuildValue("i",height));
+
 	if (!function)
 	{
 		printf("PyObject_GetAttrString fun error\n");
@@ -17,18 +29,170 @@ int main(int argc, char *argv[])
 
 	int a = 20;
 	int b = 30;
-	PyObject *args = PyTuple_New(2);
-	PyTuple_SetItem(args,0,Py_BuildValue("i",a));
-	PyTuple_SetItem(args,1,Py_BuildValue("i",b));
+	PyObject *args = Py_BuildValue("(ii)",a,b);
 
 	PyObject *RetValue = PyObject_CallObject(function, args);
 
+	
+	
 	long retval = PyLong_AsLong(RetValue);
 	printf("retval = %ld\n",retval);
 
 	Py_DECREF(args);
 	Py_DECREF(function);
 	Py_DECREF(module);
+
+	Py_Finalize();
+
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	Py_Initialize();
+	PyRun_SimpleString("import os,sys");
+	PyRun_SimpleString("sys.path.append('./')");
+
+	PyObject *module = PyImport_ImportModule("pytest");
+
+	PyObject *str = Py_BuildValue("s","asb");
+	PyObject *list = PyObject_GetAttrString(module,"testlist");
+
+	int n = PyList_Size(list);
+	for (int i = 0;i < n;i++)
+	{
+		PyObject *item = PyList_GetItem(list,i);
+		if (PyLong_Check(item))
+		{
+			long val = PyLong_AsLong(item);
+			printf("%ld\n",val);
+		}
+	}
+
+	Py_Finalize();
+
+	return 0;
+}
+
+int testtuple(int argc, char *argv[])
+{
+	Py_Initialize();
+	PyRun_SimpleString("import os,sys");
+	PyRun_SimpleString("sys.path.append('./')");
+
+	PyObject *module = PyImport_ImportModule("pytest");
+
+	PyObject *list = PyObject_GetAttrString(module,"testtuple");
+
+	int n = PyTuple_Size(list);
+	for (int i = 0;i < n;i++)
+	{
+		PyObject *item = PyTuple_GetItem(list,i);
+		if (PyUnicode_Check(item))
+		{
+			wchar_t *val = PyUnicode_AsUnicode(item);
+			wprintf(L"%ls\n",val);
+		}
+	}
+
+	Py_Finalize();
+
+	return 0;
+}
+
+int testdict(int argc, char *argv[])
+{
+	Py_Initialize();
+	PyRun_SimpleString("import os,sys");
+	PyRun_SimpleString("sys.path.append('./')");
+
+	PyObject *module = PyImport_ImportModule("pytest");
+
+	PyObject *list = PyObject_GetAttrString(module,"testdict");
+	
+	PyObject *key = NULL;
+	PyObject *value = NULL;
+	int n = PyDict_Size(list);
+	for (int i = 0;i < n;i++)
+	{
+		Py_ssize_t pos = i;
+		PyDict_Next(list, &pos, &key, &value);
+		if (PyUnicode_Check(key))
+		{
+			wchar_t *val = PyUnicode_AsUnicode(key);
+			wprintf(L"%ls\n",val);
+		}
+
+		if (PyLong_Check(value))
+		{
+			long val = PyLong_AsLong(value);
+			wprintf(L"%ld\n",val);
+		}
+	}
+
+	Py_Finalize();
+
+	return 0;
+}
+
+int basevals(int argc, char *argv[])
+{
+	Py_Initialize();
+
+	PyRun_SimpleString("import os,sys");
+	PyRun_SimpleString("sys.path.append('./')");
+
+	PyObject *module = PyImport_ImportModule("pytest");
+
+	PyObject *widthobj = PyObject_GetAttrString(module,"width");
+	if (PyLong_Check(widthobj))
+	{
+		long width = PyLong_AsLong(widthobj);
+		printf("width = %ld\n",width);
+	}
+
+	PyObject *teststrobj = PyObject_GetAttrString(module,"teststr");
+	if (PyUnicode_Check(teststrobj))
+	{
+		wchar_t *teststr = PyUnicode_AsUnicode(teststrobj);
+		printf("teststr = %ls\n",teststr);
+	}
+
+	PyObject *testfloatobj = PyObject_GetAttrString(module,"testfloat");
+	if (PyFloat_Check(testfloatobj))
+	{
+		double testfloat = PyFloat_AsDouble(testfloatobj);
+		printf("testfloat = %lf\n",testfloat);
+	}
+
+	Py_Finalize();
+
+	return 0;
+}
+
+int setvals(int argc, char *argv[])
+{
+	Py_Initialize();
+
+	PyRun_SimpleString("import os,sys");
+	PyRun_SimpleString("sys.path.append('./')");
+
+	PyObject *module = PyImport_ImportModule("pytest");
+
+	PyObject_SetAttrString(module,"mki",Py_BuildValue("i",1));
+	PyObject_SetAttrString(module,"mkd",Py_BuildValue("d",1.1));
+	PyObject_SetAttrString(module,"mks",Py_BuildValue("s","aaaa"));
+
+	PyObject_SetAttrString(module,"mktuple",Py_BuildValue("(ii)",1,2));
+	PyObject_SetAttrString(module,"mklist",Py_BuildValue("[ii]",1,2));
+
+	PyObject *function = PyObject_GetAttrString(module,"mk");
+	PyObject *args = Py_BuildValue("(ii)",1,2);
+
+	if (function)
+	{
+		PyObject_CallObject(function, args);
+	}
 
 	Py_Finalize();
 
